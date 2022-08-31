e6e9374d6fdeef4b9e1320cf58257b3c906a9782
hjeong2020 <hyesun.jeong@utexas.edu>
2022-08-31 12:16:02 -0500

    post

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,15 @@
 import {SnackbarProvider} from 'notistack';
 import MyButton from './components/MyButton';
+import Main from './components/Main'
 
 const App = () => {
   return (
     <div>
+      <Main posts="POST TEST" title="title test" />
+
       <SnackbarProvider>
         <MyButton />
+        
       </SnackbarProvider>
 
     </div>
